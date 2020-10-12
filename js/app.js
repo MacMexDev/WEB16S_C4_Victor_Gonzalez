02d7b611bc95a4d23325bad9341ba92207ea3412
MacMexDev <exodo.mac@gmail.com>
2020-10-12 08:20:30 -0500

    2,3,5,6 ok
    Desarrollo de metodos para cubrir los puntos 2,3,5 y 6, pulsar teclas, agregar numeros al display, controlar el cero, eliminar numeros con On.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-var calculadora = {
-
-	init: function(){
-
-	this.asignar_pulsar();
-
-	},
+var Calculadora = {
 
 	asignar_pulsar: function(){
 
-		pteclas = document.getElementsByClassName('tecla');
+		pteclas = document.querySelectorAll('[class~=tecla]');
 
 		for (var i = 0; i < pteclas.length; i++) {
-			// pteclas[i].onclick = this.pulsar;
+
 			pteclas[i].onmousedown = this.presionar_tecla;
 			pteclas[i].onmouseup = this.soltar_tecla;
+
+			pteclas[i].addEventListener("click",this.add_number);
+			
 		}
 
 	},
+
 	presionar_tecla: function(element){
 
 		vw = element.target.width - 3;
@@ -26,6 +24,7 @@ var calculadora = {
 		element.target.style.width = vw+'px';
 		
 	},
+
 	soltar_tecla: function(element){
 
 		vw = element.target.width + 3;
@@ -33,6 +32,24 @@ var calculadora = {
 		
 		element.target.style.height = vh+'px';
 		element.target.style.width = vw+'px';
+
+	},
+
+	add_number: function(element){
+
+		var vdisplay = document.getElementById("display").innerHTML;
+
+		var vnum = element.target.alt;
+
+		if (vnum == '1' || vnum == '2' || vnum == '3' || vnum == '4' || vnum == '5' || vnum == '6' || vnum == '7' || vnum == '8' || vnum == '9' || vnum == '0' ) {
+			if (vdisplay == '0') {
+				document.querySelector('#display').innerHTML = vnum;
+			} else {
+				document.querySelector('#display').innerHTML = document.querySelector('#display').innerHTML + vnum;
+			}
+		} else if (vnum == 'On') {
+			document.querySelector('#display').innerHTML = '0';
+		}
 
 	},
 
@@ -52,7 +69,12 @@ var calculadora = {
 		var out = a/b
 		return out;
 	},
+	init: function(){
+
+	this.asignar_pulsar();
+
+	},
 
 }
 
-calculadora.init();
+Calculadora.init();
