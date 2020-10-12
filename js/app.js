77d6a7c8abb8a8f04758a7bca961dce282c864a5
MacMexDev <exodo.mac@gmail.com>
2020-10-12 11:45:50 -0500

    revisionde metodo

--- js/app.js
+++ js/app.js
@@ -1,47 +1,53 @@
 var Calculadora = {
 
-	asignar_pulsar: function(){
+	init: function(){
 
-		pteclas = document.querySelectorAll('[class~=tecla]');
+	this.asignarTriggerTecla();
+
+	},
+
+	asignarTriggerTecla: function(){
+
+		var pteclas = document.querySelectorAll("[class~='tecla']");
 
 		for (var i = 0; i < pteclas.length; i++) {
 
-			pteclas[i].onmousedown = this.presionar_tecla;
-			pteclas[i].onmouseup = this.soltar_tecla;
+			pteclas[i].onmousedown = this.presionarTecla;
+			pteclas[i].onmouseup = this.soltarTecla;
 
-			pteclas[i].addEventListener("click",this.add_number);
+			pteclas[i].addEventListener("click",this.addNumber);
 			
 		}
 
 	},
 
-	presionar_tecla: function(element){
+	presionarTecla: function(element){
 
-		vw = element.target.width - 3;
-		vh = element.target.height - 3;
+		var vw = element.target.width - 3;
+		var vh = element.target.height - 3;
 		
 		element.target.style.height = vh+'px';
 		element.target.style.width = vw+'px';
 		
 	},
 
-	soltar_tecla: function(element){
+	soltarTecla: function(element){
 
-		vw = element.target.width + 3;
-		vh = element.target.height + 3;
+		var vw = element.target.width + 3;
+		var vh = element.target.height + 3;
 		
 		element.target.style.height = vh+'px';
 		element.target.style.width = vw+'px';
 
 	},
 
-	add_number: function(element){
+	addNumber: function(element){
 
 		var vdisplay = document.getElementById("display").innerHTML;
 
 		var vnum = element.target.alt;
 
-		if (vnum == '1' || vnum == '2' || vnum == '3' || vnum == '4' || vnum == '5' || vnum == '6' || vnum == '7' || vnum == '8' || vnum == '9' || vnum == '0' ) {
+		if (this.validaNumero(vnum)) {
 			if (vdisplay == '0') {
 				document.querySelector('#display').innerHTML = vnum;
 			} else {
@@ -53,28 +59,31 @@ var Calculadora = {
 
 	},
 
+	validaNumero: function(vnum){
+		var out;
+		if (vnum == '1' || vnum == '2' || vnum == '3' || vnum == '4' || vnum == '5' || vnum == '6' || vnum == '7' || vnum == '8' || vnum == '9' || vnum == '0' ) {
+			out = true;
+		}
+		return out;
+	},
+
 	suma: function(a,b){
-		var out = a+b
+		var out = a+b;
 		return out;
 	},
 	resta: function(a,b){
-		var out = a-b
+		var out = a-b;
 		return out;
 	},
 	multiplica: function(a,b){
-		var out = a*b
+		var out = a*b;
 		return out;
 	},
 	divide: function(a,b){
-		var out = a/b
+		var out = a/b;
 		return out;
-	},
-	init: function(){
+	}
 
-	this.asignar_pulsar();
-
-	},
-
-}
+};
 
 Calculadora.init();
